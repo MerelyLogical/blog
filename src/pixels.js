da8a461392c45f236ab17be01b8d899f3e36796f
MerelyLogical <qieerbushe7@gmail.com>
2022-04-01 01:15:52 +0100

    font format cleanup

--- src/pixels.js
+++ src/pixels.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 
 
 var ps = 3; // pixel scaling (only used in final drawing)
-var sd = []; // screen data per scaled pixel
+var sd = [] // screen data per scaled pixel
 
 var bw = 7; // block width in scaled pixels
 var bh = 7; // block height in scaled pixels
@@ -15,7 +15,7 @@ var sh = 30 * bh; // screen height
 
 function clearscreen(sd) {
     for (var i = 0; i < sh * sw; i++) {
-        sd[i] = 0;
+        sd[i] = '.';
     }
 }
 
@@ -94,40 +94,48 @@ function itoxy(i) {
     return [x, y];
 }
 
-function drawscreen(s_data) {
+function drawscreen(sd) {
     for (var i = 0; i < sh * sw; i++) {
         var [x, y] = itoxy(i);
-        if (s_data[i] === 0) {
+        if (sd[i] === '.') {
             ctx.fillStyle = "#000000" // black
             ctx.fillRect(x * ps, y * ps, ps, ps);
-        } else if (s_data[i] === 1) {
+        } else if (sd[i] === 'w') {
             ctx.fillStyle = "#FFFFFF" // white
             ctx.fillRect(x * ps, y * ps, ps, ps);
-        } else if (s_data[i] === 2) {
+        } else if (sd[i] === 'r') {
             ctx.fillStyle = "#FF0000" // red
             ctx.fillRect(x * ps, y * ps, ps, ps);
-        } else if (s_data[i] === 3) {
+        } else if (sd[i] === 'g') {
             ctx.fillStyle = "#00FF00" // green
             ctx.fillRect(x * ps, y * ps, ps, ps);
-        } else if (s_data[i] === 4) {
+        } else if (sd[i] === 'b') {
             ctx.fillStyle = "#0000FF" // blue
             ctx.fillRect(x * ps, y * ps, ps, ps);
-        } else if (s_data[i] === 5) {
+        } else if (sd[i] === 'y') {
             ctx.fillStyle = "#FFFF00" // yellow
             ctx.fillRect(x * ps, y * ps, ps, ps);
-        } else if (s_data[i] === 6) {
+        } else if (sd[i] === 'm') {
             ctx.fillStyle = "#FF00FF" // magenta
             ctx.fillRect(x * ps, y * ps, ps, ps);
-        } else if (s_data[i] === 7) {
+        } else if (sd[i] === 'c') {
             ctx.fillStyle = "#00FFFF" // cyan
+            ctx.fillRect(x * ps, y * ps, ps, ps);
+        } else if (sd[i] === 'o') {
+            ctx.fillStyle = "#FFA500" // orange
+            ctx.fillRect(x * ps, y * ps, ps, ps);
+        } else if (sd[i] === 'v') {
+            ctx.fillStyle = "#7F00FF" // violet
             ctx.fillRect(x * ps, y * ps, ps, ps);
         }
     }
 }
 
 // TODO clean this up
-var fx = 0;
-var fy = 0;
+// make a character move
+// have a initialisation function maybe
+var fx = 5;
+var fy = 5;
 
 function keyhandler (e) {
     switch (e.key) {
@@ -158,19 +166,17 @@ for (var i = 0; i < 11; i++) {
     writeletter(sd, bw * (i + 1), bh, str[i]);
 }
 
-// flag
-writeletter(sd, 13 * bw, 1 * bh, 'flag');
-
 // test rainbow
+var colors = '.wrgbymc...roygcbv'
 for (var i = 0; i < 100; i++) {
-    for (var j = 0; j < 8; j++) {
-        sd[(50 + (2 * j)) * sw + (bw + i)] = j;
-        sd[(50 + (2 * j + 1)) * sw + (bw + i)] = j;
+    for (var j = 0; j < colors.length; j++) {
+        sd[(50 + (2 * j)) * sw + (bw + i)] = colors[j];
+        sd[(50 + (2 * j + 1)) * sw + (bw + i)] = colors[j];
     }
 }
 
-writeletter(sd, 5 * bw, 8 * bh, 'flag');
-writeletter(sd, 7 * bw, 8 * bh, 'flag');
-writeletter(sd, 9 * bw, 8 * bh, 'flag');
+// starting flag position
+writeletter(sd, fx * bw, fy * bh, 'flag');
+
 // showblockgrids();
 drawscreen(sd);
