9ed484108b64720005b5170d3a4c26502b95cc9c
MerelyLogical <qieerbushe7@gmail.com>
2024-05-24 15:56:23 +0100

    customisable size

--- pages/ecalc.md
+++ pages/ecalc.md
@@ -5,11 +5,15 @@ nav_order: 15
 ---
 
 {: .fs-6}
-Estimates <i>e</i> by Simulating Many People Sitting on a Long Bench
+Estimates <i>e</i> by Simulating People Sitting on a Long Bench
 
 e is <span id="e"></span>
 
-<strong>Inspired by <a href="https://www.youtube.com/watch?v=4vxSyqk0jVY">｢競技プログラミング史上最難問【ゆっくり解説】</a>.
+<form>
+    <input id="sizeinput" type="number" min="5" value="10000" onchange="main()">
+</form>
+
+<strong>Inspired by <a href="https://www.youtube.com/watch?v=4vxSyqk0jVY">競技プログラミング史上最難問【ゆっくり解説】</a>.
 
 bench looks like: <span id="bench"></span>
 
--- src/ecalc.js
+++ src/ecalc.js
@@ -1,43 +1,59 @@
-const BENCH_SIZE = 10000;
-
-// TODO: cleanup code
-// TODO: add buttons to increase bench_size
-
-var order = [...Array(BENCH_SIZE).keys()];
-var bench = Array(BENCH_SIZE).fill(false);
-var count = 0;
-
-let i = BENCH_SIZE;
-while (i > 0) {
-  let j = Math.floor(Math.random() * i);
-  i--;
-  [order[i], order[j]] = [order[j], order[i]];
+// shuffle seating order
+function shuffle(arr) {
+    let i = arr.length;
+    while (i > 0) {
+        let j = Math.floor(Math.random() * i);
+        i--;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
 }
 
-for (i = 0; i < BENCH_SIZE; i++) {
-  let sit = order[i];
-  if (sit == 0) {
-    if (!bench[1]) { bench[sit] = true; count++; }
-  } else if (sit == BENCH_SIZE-1) {
-    if (!bench[BENCH_SIZE-2]) { bench[sit] = true; count++; }
-  } else {
-    if (!bench[sit-1] && !bench[sit+1]) { bench[sit] = true; count++; }
-  }
+function main() {
+    let size = parseInt(document.getElementById('sizeinput').value);
+    let order = [...Array(size).keys()];
+    let bench = Array(size).fill(false);
+    let count = 0;
+    
+    shuffle(order);
+
+    // sit people in
+    for (let i = 0; i < size; i++) {
+        let sit = order[i];
+        if (sit == 0) {
+            if (!bench[1]) {
+                bench[sit] = true;
+                count++;
+            }
+        } else if (sit == size - 1) {
+            if (!bench[size - 2]) {
+                bench[sit] = true;
+                count++;
+            }
+        } else {
+            if (!bench[sit - 1] && !bench[sit + 1]) {
+                bench[sit] = true;
+                count++;
+            }
+        }
+    }
+    
+    // draw bench
+    let benchString = "";
+    for (let i = 0; i < size; i++) {
+        if (bench[i]) {
+            benchString += "o";
+        } else {
+            benchString += "_";
+        }
+    }
+    
+    document.getElementById('bench').innerHTML = benchString;
+    
+    // calculate e
+    let p = count / size;
+    let e = 1 / (Math.sqrt(1 - (2 * p)));
+    
+    document.getElementById('e').innerHTML = e;
 }
 
-var benchString = "";
-for (i = 0; i < BENCH_SIZE; i++) {
-  if (bench[i]) {
-    benchString += "o";
-  } else {
-    benchString += "_";
-  }
-}
-
-document.getElementById('bench').innerHTML = benchString;
-
-let p = count / BENCH_SIZE;
-let e = 1 / (Math.sqrt(1 - (2 * p)));
-
-document.getElementById('e').innerHTML = e;
-
+main();
