8c38fcff9b94f1ac3fd707dfa93b8356701d86e5
MerelyLogical <qieerbushe7@gmail.com>
2025-09-09 20:21:42 +0100

    fix moment

---
+++ content/_meta.ts
@@ -0,0 +1,3 @@
+export default {
+    "moment": "6 - A Moment of Time"
+}
--- js/moment.js
+++ js/moment.js
@@ -1,9 +1,13 @@
-// roughly where Big Ben is
-var today = new Date(),
-    times = SunCalc.getTimes(today, 51.501, -0.125, 11),
-    sunrise = times.sunrise,
-    sunset = times.sunset;
-    mlen = (sunset - sunrise) / 480000
+import { SunCalcGetTimes } from "./suncalc/suncalc.js"
+
+export function moment() {
+    // roughly where Big Ben is
+    let today = new Date()
+    let times = SunCalcGetTimes(today, 51.501, -0.125, 11)
+    let sunrise = times.sunrise
+    let sunset = times.sunset
+    let mlen = (sunset - sunrise) / 480000
 
     // TODO: read coord from user and calculate
-document.getElementById('moment').innerHTML = mlen.toFixed(1);
+    return mlen.toFixed(1);
+}
