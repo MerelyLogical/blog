77b3ecebb0793399b3672d84cb2c8cbea29eedf7
MerelyLogical <qieerbushe7@gmail.com>
2025-09-09 20:31:14 +0100

    sort navorders

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,3 +1,49 @@
 export default {
-    "moment": "6 - A Moment of Time"
+    "clocks": {
+        title: "Clocks",
+    },
+    "dakuten": {
+        title: "Dakuten",
+    },
+    "hex": {
+        title: "Hexagonal Grid",
+    },
+    "hex2": {
+        title: "Hexagonal Grid Pathfinding",
+    },
+    "moment": {
+        title: "A Moment of Time",
+    },
+    "aruaru": {
+        title: "Aruaru Quiz",
+    },
+    "nas": {
+        title: "NAS Build Log",
+    },
+    "picalc": {
+        title: "Pi Calculator",
+    },
+    "pinyin": {
+        title: "Pinyin Chart",
+    },
+    "mapgen": {
+        title: "Random Map Pathfinding",
+    },
+    "marketsim": {
+        title: "Stock Market Simulator",
+    },
+    "physics": {
+        title: "Physics Engine",
+    },
+    "ecalc": {
+        title: "e Calculator",
+    },
+    "pixels": {
+        title: "Pi Player",
+        display: 'hidden'
+    },
+    "404": {
+        title: "404",
+        display: 'hidden'
+    }
 }
