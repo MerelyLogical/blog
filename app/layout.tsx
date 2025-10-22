9a0426ebd367ad6df970bc1943574f9bccc4b579
MerelyLogical <qieerbushe7@gmail.com>
2025-10-22 22:03:46 +0100

    fix github action issue and maybe image loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import {Footer, Layout, Navbar} from 'nextra-theme-docs'
-import {Banner, Head} from 'nextra/components'
-import {getPageMap} from 'nextra/page-map'
+import { Layout, Navbar} from 'nextra-theme-docs'
+import { Head} from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
-import Link from "next/link";
 
 export const metadata = {
     // Define your metadata here
@@ -12,7 +11,7 @@ export const metadata = {
 const navbar = (
     <Navbar logo={
         <>
-            <img src="images/logo.jpg" alt="Logo" width={100} height={20} />
+            <img src="@/images/logo.jpg" alt="Logo" width={100} height={20} />
             <span style={{ marginLeft: '1em', fontWeight: 600 }}>
                 ML's BF's Website!
             </span>
@@ -32,7 +31,7 @@ export default async function RootLayout({children}) {
         suppressHydrationWarning
         >
         <Head>
-            <link rel="shortcut icon" href="images/favicon.ico"/>
+            <link rel="shortcut icon" href="@/images/favicon.ico"/>
         </Head>
         <body>
         <Layout
