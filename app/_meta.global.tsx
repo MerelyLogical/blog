c390a01e5b347e976377d28e5a364c7780ebb0fd
MerelyLogical <qieerbushe7@gmail.com>
2025-10-26 23:07:09 +0000

    add last update time to letsplay

--- app/_meta.global.tsx
+++ app/_meta.global.tsx
@@ -30,7 +30,7 @@ export default {
             "nas0": { title: "0 - Building a Server"},
             "nas1": { title: "1 - Building a NAS" },
             "nas2": { title: "2 - Improving the NAS" },
-            "nas3": { title: "3 - Optimising the NAS" },
+            "nas3": { title: "3 - Upgrading the NAS" },
         },
     },
     '--': {
